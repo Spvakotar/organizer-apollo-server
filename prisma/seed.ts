978a89428eb7b69763b9f69adfe74e3bdefc1506
Spvakotar <spvakotar@outlook.com>
2022-07-21 11:03:44 +0000

    update seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,24 +1,27 @@
 import { PrismaClient } from '@prisma/client'
-import faker from 'faker'
+//import faker from 'faker'
 
 const prisma = new PrismaClient()
 
-const NUMBER_OF_USERS = 4
-const MAX_NUMBER_OF_LINKS = 5
+//const NUMBER_OF_USERS = 4
+//const MAX_NUMBER_OF_LINKS = 5
 
-const data = Array.from({ length: NUMBER_OF_USERS }).map(() => ({
-  email: faker.internet.email(),
-  name: faker.name.firstName(),
-  links: Array.from({
-    length: faker.datatype.number({
-      min: 0,
-      max: MAX_NUMBER_OF_LINKS,
-    }),
-  }).map(() => ({
-    url: faker.internet.url(),
-    shortUrl: faker.internet.domainWord(),
-  })),
-}))
+// const data = Array.from({ length: NUMBER_OF_USERS }).map(() => ({
+//   email: faker.internet.email(),
+//   name: faker.name.firstName(),
+//   links: Array.from({
+//     length: faker.datatype.number({
+//       min: 0,
+//       max: MAX_NUMBER_OF_LINKS,
+//     }),
+//   }).map(() => ({
+//     url: faker.internet.url(),
+//     shortUrl: faker.internet.domainWord(),
+//   })),
+// }))
+const data = [
+  {name:"Sunil", email:"sp@vak.com", links:[{url:"http://norris.info", shortUrl:"norris"}]}
+];
 
 async function main() {
   for (let entry of data) {
